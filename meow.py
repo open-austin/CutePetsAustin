921fc330d654f3b5a2beeccf6dcf1a3ea2fd4d71
Luqmaan <ldawoodjee@gmail.com>
2014-08-12 13:26:44 -0500

    Having some data issues, none of the pets from today are in PetHarbor
    GET http://data.austintexas.gov/resource/5cgv-i2cu.json?$where=intake_type>='2014-08-12' AND intake_type<'2014-08-13'
    Found 23 pets
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A590144
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685729
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685730
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685731
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685732
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685733
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685734
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685737
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685740
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685747
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685751
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685752
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685753
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685754
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685756
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685760
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685761
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685762
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685763
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685764
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685765
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685766
    404 http://www.petharbor.com/pet.asp?uaid=ASTN.A685768
    0 of 23 pets still exist in PetHarbor

--- meow.py
+++ meow.py
@@ -25,32 +25,39 @@ def get_pet_details(url):
     res.raise_for_status()
 
     four_oh_four = 'Sorry!  This animal is no longer in our online database.  Please check with the shelter to see about its availability.'
-    if four_oh_four not in res.content:
+
+    if four_oh_four in res.content:
+        print 404, url
+    else:
+        print 200, url
         print res.content
         in_db = True
-        media = 'lalalafind.theurl'
+        media = 'lalalafind.theurl'  # FIXME: Figure out how to get the image url
 
     return in_db, media
 
 
-intake_start_time = arrow.now().replace(hours=-2).isoformat()
-url = "http://data.austintexas.gov/resource/5cgv-i2cu.json?$where=intake_type>='{}'".format(intake_start_time)
+intake_start_time = arrow.now().format('YYYY-MM-DD')
+intake_end_time = arrow.now().replace(days=1).format('YYYY-MM-DD')  # Doubt there will be a date in the future, but just in case
+url = "http://data.austintexas.gov/resource/5cgv-i2cu.json?$where=intake_type>='{}' AND intake_type<'{}'".format(intake_start_time, intake_end_time)
 print 'GET', url
 res = requests.get(url)
+res.raise_for_status()
 pets = res.json()
-potential_pets = []
+print 'Found {} pets'.format(len(pets))
+existing_pets = []
 
 for pet in pets:
-    in_db, media = get_pet_details(url)
+    in_db, media = get_pet_details(pet['image_link']['url'])
 
     if in_db:
-        potential_pets.append(pet)
+        existing_pets.append(pet)
 
-if not potential_pets:
-    print 'No pets found'
-else:
-    lucky = random.choice(potential_pets)
+print '{} of {} pets still exist in PetHarbor'.format(len(existing_pets), len(pets))
+
+if existing_pets:
+    lucky = random.choice(existing_pets)
     latlng = (lucky['found_location']['latitude'], lucky['found_location']['longitude'])
     status = '{} {} {} {}'.format(lucky['color'], lucky['looks_like'], lucky['type'], lucky['image_link']['url'])
     print 'Chosen one:', lucky
-    tweet(status, latlng, media)
+    # tweet(status, latlng, media)
